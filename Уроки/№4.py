7fec4bd664722cd7477e4e7e4c486c625ef9625b
eldboy <vlad-rudskoy2003@bk.ru>
2022-08-17 06:52:24 +0300

    first commit

---
+++ Уроки/№4.py
@@ -0,0 +1,41 @@
+# Уроки Python с нуля / #4 – Переменные и типы данных
+# Переменные
+# -------------------------------------
+# number = 7  # int
+
+# del number
+
+# number = 5  # int
+# print("Результат:", number)
+
+
+# digit = -4.54356876  # float
+# word = "Результат: "  # string
+# boolean = True  # bool
+# str_num = '5' # string
+
+# print(word + str(digit))
+
+# print(word + str(number + int(str_num)))
+
+
+# Работа с пользователем
+num1 = int(input("Введите первое число: "))
+# string
+num2 = int(input("Введите второе число: "))
+
+num1 *= 5
+
+print("Result:", num1 + num2)
+print("Result:", num1 - num2)
+print("Result:", num1 / num2)
+print("Result:", num1 * num2)
+print("Result:", num1 ** num2)
+print("Result:", num1 // num2)
+
+word = "Hi"  # string
+print(word * 2)
+
+word = True
+
+# Конец!
